06019e1260523f488566e8181b4bfcc58b21580b
nnamacha <nnamacha@gmail.com>
2020-01-11 12:57:12 +0000

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, UsersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
